920ed78234b00ea3a7fec0bad03233f0adb83837
junjuny0227 <junjuny0227@gmail.com>
2025-11-05 20:12:07 +0900

    린팅 오류

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -1,5 +1,4 @@
 import type { PropsWithChildren } from 'react';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContainer } from 'react-toastify';
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-
 import App from './App.tsx';
 import Provider from './lib/provider.tsx';
 import GlobalStyle from './styles/GlobalStyle.tsx';
