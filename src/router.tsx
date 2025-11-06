8a765277fd2226f8ec81b1ad45fac38f1065e156
junjuny0227 <junjuny0227@gmail.com>
2025-11-06 15:25:31 +0900

    feat: session 발급용 시작 페이지

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,21 @@
 // /src/router.tsx
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 import Layout from './layout';
-import { Chat, Main, Report, ReportDetail } from './pages';
+import { Chat, Main, Report, ReportDetail, Start } from './pages';
 
 export const router = createBrowserRouter([
   {
+    path: '/',
+    element: <Navigate to="/start" replace />,
+  },
+  {
+    path: '/start',
+    element: <Start />,
+  },
+  {
     element: <Layout />,
     children: [
-      { path: '/', element: <Main /> },
+      { path: '/main', element: <Main /> },
       { path: '/report', element: <Report /> },
       { path: '/report/:id', element: <ReportDetail /> },
       { path: '/chat', element: <Chat /> },
