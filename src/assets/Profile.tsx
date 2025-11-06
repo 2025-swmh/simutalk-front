4bd5d5b884c83ddf3ddbf19a2c211902bd910c7d
junjuny0227 <junjuny0227@gmail.com>
2025-11-06 14:01:23 +0900

    feat: 에셋 컴포넌트 추가

---
+++ src/assets/Profile.tsx
@@ -0,0 +1,21 @@
+const Profile = () => (
+  <svg
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+  >
+    <circle cx="20" cy="20" r="19.5" fill="#F4F4F5" stroke="#52525B"></circle>
+    <path
+      d="M27 29V27C27 25.9391 26.5786 24.9217 25.8284 24.1716C25.0783 23.4214 24.0609 23 23 23H17C15.9391 23 14.9217 23.4214 14.1716 24.1716C13.4214 24.9217 13 25.9391 13 27V29M24 15C24 17.2091 22.2091 19 20 19C17.7909 19 16 17.2091 16 15C16 12.7909 17.7909 11 20 11C22.2091 11 24 12.7909 24 15Z"
+      stroke="#52525B"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
+
+export default Profile;
