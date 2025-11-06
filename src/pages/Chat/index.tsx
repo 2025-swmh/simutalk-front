699957385666eed1deefa8679b1cc519d4e166e6
junjuny0227 <junjuny0227@gmail.com>
2025-11-06 16:04:49 +0900

    fix: 채팅 페이지 ui 변경

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,30 +1,23 @@
 import { Check } from '../../assets';
-import ChatListItem from '../../components/ChatListItem';
 import * as S from './style';
 
 const Chat = () => {
-  const chatList = [
-    {
-      id: 1,
-      name: '홍길동',
-      message: '안녕하세요! 프론트엔드 개발자와의 협업에 대해 이야기해봐요.',
-    },
-  ];
-
   return (
     <S.ChatContainer>
-      <S.ChatListContainer>
-        {chatList.map((chat) => (
-          <ChatListItem key={chat.id} name={chat.name} message={chat.message} />
-        ))}
-      </S.ChatListContainer>
-      <S.RightColumn>
+      <S.ChatContent>
         <S.ChatDetailContainer>
           <S.SpeechLabel>
-            홍길동
+            AI
             <S.AiSpeech>안녕하세요! 무엇을 도와드릴까요?</S.AiSpeech>
           </S.SpeechLabel>
           <S.UserSpeech>안녕하세요! 프론트엔드 개발자와의 협업에 대해 이야기해봐요.</S.UserSpeech>
+          <S.SpeechLabel>
+            AI
+            <S.AiSpeech>
+              좋아요! 프론트엔드 개발자와 협업할 때 가장 중요한 것 중 하나는 API 설계와 데이터
+              구조에 대한 이해입니다. 현재 어떤 프로젝트를 진행하고 계신가요?
+            </S.AiSpeech>
+          </S.SpeechLabel>
         </S.ChatDetailContainer>
         <S.InputContainer>
           <S.ChatInput placeholder="여기에 입력해주세요." />
@@ -32,7 +25,7 @@ const Chat = () => {
             <Check />
           </S.SendButton>
         </S.InputContainer>
-      </S.RightColumn>
+      </S.ChatContent>
     </S.ChatContainer>
   );
 };
