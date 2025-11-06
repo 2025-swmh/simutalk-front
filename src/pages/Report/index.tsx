9c9ef95c419575e725dafe837a2f22fba1755028
seungW0o <ssounsowns@gmail.com>
2025-11-06 17:57:37 +0900

    fix: lint error

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
