e863fcfb5c3f9a278e5c200752d26883c25c849a
seungW0o <ssounsowns@gmail.com>
2025-11-06 09:31:24 +0900

    WIP on feat/emptyReport

--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,5 +1,15 @@
+import { EmptyReport } from '../../components';
+import * as S from './style';
+
 const Report = () => {
-  return <div>Report Page</div>;
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.Title>내 보고서</S.Title>
+        <EmptyReport />;
+      </S.Container>
+    </S.Wrapper>
+  );
 };
 
 export default Report;
