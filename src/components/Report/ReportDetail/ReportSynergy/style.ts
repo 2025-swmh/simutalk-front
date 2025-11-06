138b1f3820c8621527b9050a03189fbf09d888e6
seungW0o <ssounsowns@gmail.com>
2025-11-06 14:36:33 +0900

    feat: ReportSynergy 컴포넌트 추가

---
+++ src/components/Report/ReportDetail/ReportSynergy/style.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import { theme } from '../../../../styles';
+
+export const SynergyTitle = styled.div`
+  display: flex;
+  gap: 10px;
+  ${theme.font.title1}
+`;
+export const SynergyContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 7px;
+`;
+export const SynergyContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
